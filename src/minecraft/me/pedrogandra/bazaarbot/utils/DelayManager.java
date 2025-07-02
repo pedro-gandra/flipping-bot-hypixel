e52ded637d8dc27bfeda34e1a3f1fe1d95ae64e7
Pedro Gandra <pedro.gandra.andrade@gmail.com>
2025-07-02 13:40:03 -0300

    Implementação de system wide delay manager

---
+++ src/minecraft/me/pedrogandra/bazaarbot/utils/DelayManager.java
@@ -0,0 +1,47 @@
+package me.pedrogandra.bazaarbot.utils;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DelayManager {
+
+	public static DelayManager instance = new DelayManager();
+	private final Queue<Task> tasks = new LinkedList<Task>();
+	
+	private static class Task {
+        Runnable action;
+        long executeAt;
+
+        Task(Runnable action, long executeAt) {
+            this.action = action;
+            this.executeAt = executeAt;
+        }
+    }
+	
+	public synchronized void schedule(Runnable runnable, long delayMillis) {
+        long executeTime = System.currentTimeMillis() + delayMillis;
+
+        if (!tasks.isEmpty()) {
+            Task last = null;
+            for (Task t : tasks) last = t;
+            if (last != null && last.executeAt > executeTime) {
+                executeTime = last.executeAt + delayMillis;
+            }
+        }
+
+        tasks.add(new Task(runnable, executeTime));
+    
+	}
+	
+	public synchronized void onTick() {
+        long now = System.currentTimeMillis();
+        while (!tasks.isEmpty() && tasks.peek().executeAt <= now) {
+            tasks.poll().action.run();
+        }
+    }
+	
+	public synchronized void clear() {
+        tasks.clear();
+    }
+	
+}
